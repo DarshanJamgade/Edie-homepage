98dda290fefe0c3d7558e62894b59921f3638068
Darshan Jamgade <darshanjamgade289@gmail.com>
2021-08-12 19:13:06 +0530

    added till projects section

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('[data-menu]');
+const navList = document.querySelector('[data-nav-list]');
+
+
+menu.addEventListener('click', () => {
+    navList.classList.toggle('show')
+})
